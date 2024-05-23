8b49e71a0c7e97092c1d1e1ac4026461e16b9097
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2024-05-23 09:14:50 -0400

    setting up rewrite

--- ac_editor.py
+++ ac_editor.py
@@ -1,9 +1,4 @@
-from src.settings import Settings
 from src.gui import start
-from src.database import Database
-import tkinter as tk
 
 if __name__ == "__main__": 
     start()
-    # gui_manager = GUIManager(tk.Tk(), Settings(), Database())
-    # gui_manager.start() 
